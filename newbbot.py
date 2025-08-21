b997be112c08159fb12b34830d52020f89b4674e
EC2 Default User <ec2-user@ip-172-31-31-172.ec2.internal>
2025-08-21 19:58:50 +0000

    update

--- newbbot.py
+++ newbbot.py
@@ -27,6 +27,9 @@ class ModBot(commands.Bot):
 
     async def setup_hook(self):
         await self.load_extension("cogs.ticketing")
+        await self.load_extension("cogs.moderation")
+
+
         # Global sync. For instant dev sync to a single guild, uncomment the next 2 lines:
         guild = discord.Object(id=1304124705896136744)
         await self.tree.sync(guild=guild)
