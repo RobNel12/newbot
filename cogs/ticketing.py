07d899f96a0e56ec972c666c04214bc047454de0
chezz <70979717+RobNel12@users.noreply.github.com>
2025-09-12 10:11:19 -0700

    Update ticketing.py

--- cogs/ticketing.py
+++ cogs/ticketing.py
@@ -450,7 +450,7 @@ class TicketChannelView(discord.ui.View):
     
         await self._lock_channel(interaction.channel, lock=False)
         self.closed = False
-        await interaction.response.send_message("🔓 Ticket reopened.", ephemeral=True)
+        await interaction.response.send_message("🔓 Ticket reopened.", ephemeral=False)
 
 
     async def _lock_channel(self, channel: discord.TextChannel, lock: bool):
